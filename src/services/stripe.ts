2c4994bdaeccaddcbf6f92da0e82a6f492b7a2e6
rheiderabe <rheiderline@gmail.com>
2021-03-31 19:10:03 -0300

    feature:oauthGIT

---
+++ src/services/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+import { version } from "../../package.json";
+
+export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: null,
+  appInfo: {
+    name: "Ignews",
+    version,
+  },
+});
